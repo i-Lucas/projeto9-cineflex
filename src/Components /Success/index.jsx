cd7edb70f3a1fd5240bb9ea88788e12a1d4e9079
i-Lucas <novo.contato.lucas@gmail.com>
2022-03-28 16:48:14 -0300

    update button home

--- src/Components /Success/index.jsx
+++ src/Components /Success/index.jsx
@@ -1,4 +1,4 @@
-import { useLocation } from 'react-router-dom'
+import { useLocation, Link } from 'react-router-dom'
 
 import Header from '../Header/'
 import './success.css'
@@ -10,23 +10,30 @@ export default function Success() {
 
     return (
         <section className="success">
+
             <Header />
+            
             <div className="success-text">
                 <h1>Pedido feito com sucesso!</h1>
             </div>
-            <div className="success-container">
 
+            <div className="success-container">
                 <h1>Filme e sessão</h1>
                 <h2>{info.title}</h2>
                 <h2>{info.date} {info.time}</h2>
 
                 <h1>Ingressos</h1>
-                <RenderSeats seats={info.seats} />      
+                <RenderSeats seats={info.seats} />
 
                 <h1>Comprador</h1>
                 <h2>Nome: {info.nome}</h2>
                 <h2>CPF: {info.cpf}</h2>
 
+                <Link style={{ textDecoration: 'none' }} to="/">
+                    <div className="btn-back">
+                        <button>Voltar pra Home</button>
+                    </div>
+                </Link>
             </div>
         </section>
     )
@@ -36,5 +43,5 @@ function RenderSeats({ seats }) {
 
     return seats.map((seat, index) =>
         <h2 key={index}>Assento {seat}</h2>
-    ) 
+    )
 }
