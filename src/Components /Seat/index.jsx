44425612c252f71574c337a398e7d9845a9be4a1
i-Lucas <novo.contato.lucas@gmail.com>
2022-03-28 03:50:49 -0300

    update selected seats system

--- src/Components /Seat/index.jsx
+++ src/Components /Seat/index.jsx
@@ -6,36 +6,36 @@ import Header from '../Header/'
 import Footer from '../Footer/'
 import './seat.css'
 
-export default function ChooseSeat () {
+export default function ChooseSeat() {
 
     const { sessionID } = useParams()
-    const [seats, setSeats] = useState([])
-    const [movieData, setMovieData] = useState([])
-    const [movieDay, setMovieDay] = useState([])
+
+    const [movieData, setData] = useState({
+
+        movie: [],
+        seats: [],
+        days: []
+    })
 
     useEffect(() => {
 
         axios.get(`https://mock-api.driven.com.br/api/v5/cineflex/showtimes/${sessionID}/seats`)
 
-            .then(({ data }) => {
-
-                setMovieData(data.movie)
-                setMovieDay(data.day)
-                setSeats(data.seats)
-            })
-            .catch(error => {
-                console.log(error)
-            })
+            .then(({ data }) => setData({ movie: data.movie, seats: data.seats, days: data.day }))
+            .catch(error => console.log(error))
 
     }, [sessionID])
 
+    const { title, posterURL: img } = movieData.movie
+    const { weekday, date } = movieData.days
     return (
+
         <section className="choose-seat">
             <Header />
             <div className="container">
                 <div className="title"><h1>Selecione o(s) assento(s)</h1></div>
                 <div className="seats">
-                    {renderSeats(seats)}
+                    <RenderSeats seats={movieData.seats} />
                 </div>
                 <div className="description">
                     <div className="selected"><div className="seat-selected"></div><h1>Selecionado</h1></div>
@@ -43,16 +43,34 @@ export default function ChooseSeat () {
                     <div className="unavailable"><div className="seat-unavailable"></div><h1>Indisponível</h1></div>
                 </div>
             </div>
-            <Footer img={movieData.posterURL} title={movieData.title} weekday={movieDay.weekday} date = {movieDay.date}/>
+            <Footer img={img} title={title} weekday={weekday} date={date} />
         </section>
     )
 }
 
-function renderSeats(seats) {  
+function RenderSeats({ seats }) {
 
-    return (
-        seats.map((seat, index) => {
-            return <div className="seat" key={index}><h1>{seat.name}</h1></div>
-        })
+    return seats.map((seat, index) =>
+        <RenderThisSeat number={seat.name} available={seat.isAvailable} key={index} />
     )
+}
+
+function RenderThisSeat({ number, available }) {
+
+    const [seatInfo, setInfo] = useState({
+        selected: false,
+        list: []
+    })
+
+    function checkSeat(number) {
+
+        if (!seatInfo.selected) {
+            setInfo({ selected: true, list: [...seatInfo.list, number] })
+        } else {
+            setInfo({ selected: false, list: seatInfo.list.filter(item => item !== number) })
+        }
+    }
+
+    return <div className={seatInfo.selected ? 'seat green' : available ? 'seat gray' : 'seat yellow'}
+        onClick={() => available ? checkSeat(number) : alert(`seat ${number} is not available`)} ><h1>{number}</h1></div>
 }
